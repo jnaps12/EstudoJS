2827ce87b1389a01ada7922e21874c15382a6143
Junior Pinheiro <paladinoshot32@gmail.com>
2021-03-19 08:54:34 -0300

    foreach

---
+++ ForEach - exemplo/script.js
@@ -0,0 +1,14 @@
+let games = ["PS4", "Switch", "XBOX", "PC"];
+
+games.forEach(function(item, index){
+  console.log(item, index, games);
+});
+
+console.log("\n");
+
+let brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+
+brasilCampeao.forEach(function(item){
+  console.log(`O Brasil foi campeão em ${item}`);
+});
+
