63f6fad1e86bda1f1e4ed0da319e3a8eaf5c321c
jnaps12 <paladinoshot32@gmail.com>
2021-10-19 10:20:07 -0300

    to-do-app sem bd simples

--- Nest/to-do-app/src/app.module.ts
+++ Nest/to-do-app/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TaskController } from './tasks/task/task.controller';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
