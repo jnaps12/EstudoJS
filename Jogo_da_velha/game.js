9c93d0277dc3cb2fba4ff39b8bda1e36ca9ab5df
Junior Pinheiro <junior.main7@gmail.com>
2022-03-23 21:49:14 -0300

    reset e reft ok

--- Jogo_da_velha/game.js
+++ Jogo_da_velha/game.js
@@ -40,9 +40,64 @@ function inicializarEspacos(){
           playTime = player1;
         }
         atualizaMostrador();
+        verificarVencedor();
       }
     });
   }
+}
 
+
+async function verificarVencedor(){
+  let a1 = document.getElementById('a1').getAttribute('jogada');
+  let a2 = document.getElementById('a2').getAttribute('jogada');
+  let a3 = document.getElementById('a3').getAttribute('jogada');
+
+  let b1 = document.getElementById('b1').getAttribute('jogada');
+  let b2 = document.getElementById('b2').getAttribute('jogada');
+  let b3 = document.getElementById('b3').getAttribute('jogada');
+
+  let c1 = document.getElementById('c1').getAttribute('jogada');
+  let c2 = document.getElementById('c2').getAttribute('jogada');
+  let c3 = document.getElementById('c3').getAttribute('jogada');
+
+  let vencedor = '';
+
+  let a1Verify = (((a1 == b1 && a1 == c1) || (a1 == a2 && a1==a3) || (a1 == b2 && a1 == c3)) && a1 != '');
+  let b2Verify = (((b2 == b1 && b2 == b3) || (b2 == a2 && b2 == c2) || (b2 == a3 && b2 == c1)) && b2 != '');
+  let c3Verify = (c3 == c2 && c3 == c1 && c3 != '') || (c3 == a3 && c3 == b3 && c3 != '');
   
+  if(a1Verify){
+    vencedor = a1;
+    console.log("a1")
+  }else if(b2Verify){
+    vencedor = b2;
+    console.log("b2");
+  }else if(c3Verify){
+    vencedor = c3;
+    console.log("c3");
+  }
+
+  if(vencedor != ''){
+    gameOver = true;
+
+    await sleep(50);
+    alert("o Ganhador foi o : '"+ vencedor + "'")
+  }
+
+}
+
+function sleep(ms){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+function reset() {
+  let espacos = document.getElementsByClassName("espaco");
+  for (const espaco of espacos) {
+    espaco.innerHTML = "";
+    espaco.setAttribute("jogada", "");
+  }
+  gameOver = false;
+  playTime = player1;
+  atualizaMostrador();
 }
