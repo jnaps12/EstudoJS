c6370c391e4bf4e19a758080c71c7a046c6f8c1e
Junior Pinheiro <junior.main7@gmail.com>
2022-05-28 11:41:21 -0300

    update

--- Nest/to-do-app/src/tasks/tasks.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TaskController } from './task/task.controller';
-import { TaskService } from './task/task.service';
-
-@Module({
-  controllers: [TaskController],
-  providers: [TaskService],
-})
-export class TasksModule {}
